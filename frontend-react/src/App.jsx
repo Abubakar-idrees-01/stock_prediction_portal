01378f4922d169ac7f5eba1ce09072a4893a466e
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-09-10 11:54:49 +0500

    lets Start

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{color: 'green', textAlign: "center" , backgroundColor:"white"} }>Start</h1>
+    </>
+  )
+}
+
+export default App
