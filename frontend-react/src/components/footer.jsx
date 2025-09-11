a7fc0413e8556109001a62ebdf510bceb14ef876
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-09-11 18:46:21 +0500

    frontend

--- frontend-react/src/components/footer.jsx
+++ frontend-react/src/components/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
     <footer className='footer py-3 my-3'>
         <hr className='border-bottom'/>
-        <p className='text-light text-center' > &copy 2025</p>
+        <p className='text-light text-center' > &copy 2025 built with ❤  by Raftaar</p>
     </footer>
     </>
   )
--- frontend-react/src/components/header.jsx
+++ frontend-react/src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import  Button from './button';
 
 
 const Header = () => {
@@ -6,9 +7,10 @@ const Header = () => {
     <nav className='navbar container pt-3 pb-3 align-items-start'>
         <a className="navbar-brand text-light" href="">Stock Prediction portal</a>
         <div>
-            <a  className='btn btn-outline-info' href="">Login</a>
+            <Button text="Login" className="btn btn-outline-info"/>
             &nbsp;
-            <a  className='btn btn-info' href="">Register</a>
+            <Button text="Register" className="btn btn-info"/>
+            
         </div>
     </nav>
   )
