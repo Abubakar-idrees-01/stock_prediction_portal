bc34b8a22837f1d255ba32127b7fe8a4080e79dc
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-09-12 18:14:22 +0500

    backend

--- backend-drf/stock_prediction_main/urls.py
+++ backend-drf/stock_prediction_main/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    
+    #base api
+    path("api/v1/",include("api.urls"))
 ]
