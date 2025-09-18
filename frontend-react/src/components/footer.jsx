89a89d421b117092951f266063bb0a48c5f6f831
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-09-18 16:32:17 +0500

    frontend

--- frontend-react/src/components/footer.jsx
+++ frontend-react/src/components/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
     <footer className='footer py-3 my-3'>
         <hr className='border-bottom'/>
-        <p className='text-light text-center' > &copy 2025 built with ❤  by Raftaar</p>
+        <p className='text-light text-center' > &copy; 2025 built with ❤  by Raftaar</p>
     </footer>
     </>
   )
