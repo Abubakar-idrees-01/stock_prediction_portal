60d46e77fb2ba0f321679e85190b6a1c2e75ff18
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-09-20 17:18:16 +0500

    frontend&backend

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {useEffect, useState} from 'react'
 
+import axiosInstance from '../../axiosInstance'
+
 const Dashboard = () => {
     useEffect(()=>{
         const fetchProtectedData = async () =>{
             try{
                 const response = await axiosInstance.get('/protected-view/');
+                // console.log(response.data)
             }catch(error){
                 console.error('Error fetching data:', error)
             }
