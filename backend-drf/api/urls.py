1f01f4ba1bfdb747c52546e3634a1f1c1ee887b1
Abubakar2-1 <abubakaridrees804@gmail.com>
2025-10-07 17:54:07 +0500

    frontend

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -9,10 +9,9 @@ urlpatterns = [
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    path('protected-view/', UserViews.ProtectedView.as_view()),
-     # Prediction API
-    path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction')
 
-    
+    path('protected-view/', UserViews.ProtectedView.as_view()),
+
+    # Prediction API
+    path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction')
 ]
